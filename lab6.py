0a2adf32e2464d611d1f95165afe4a63796add70
Jacob-Hinshaw <jrhinsha@calpoly.edu>
2024-11-02 17:55:05 -0700

    Part 4

--- lab6.py
+++ lab6.py
@@ -37,12 +37,62 @@ def selection_sort(values:list[int]) -> None:
 
 
 # Part 1
+# These functions take an input of a list of class of Books and sorts the list, alphabetically, by title.
+def index_smallest_from_alpha(values:list[data.Book], start:int) -> Optional[int]:
+    if start >= len(values) or start < 0:
+        return None
 
+    mindex = start
+    for idx in range(start + 1, len(values)):
+        if values[idx].title < values[mindex].title:
+            mindex = idx
+
+    return mindex
+
+def selection_sort_alpha(values:list[data.Book]) -> None:
+    for idx in range(len(values) - 1):
+        mindex = index_smallest_from_alpha(values, idx)
+        tmp = values[mindex]
+        values[mindex] = values[idx]
+        values[idx] = tmp
 
 # Part 2
+# This function takes an input of a str and gives an output of a str after it has swapped uppercase characters
+# to lowercase letters.
+def swap_case(input_str:str) -> str:
+    output = ""
+    for char in input_str:
+        if char.islower():
+            output += char.upper()
+        elif char.isupper():
+            output += char.lower()
+        else:
+            output += char
+    return output
 
 
 # Part 3
-
+# This function takes an input of 3 strings. The first one is the subject, the second input is a character you
+# want replaced in the subject, and the third input is what you want the old character to be replaced with.
+# This is then outputted as a string.
+def str_translate(input_str:str, old:str, new:str) -> str:
+    output_str = ""
+    for char in input_str:
+        if char == old:
+            output_str += new
+        else:
+            output_str += char
+    return output_str
 
 # Part 4
+# This function takes an input of str and outputs a count of how many times each word in the string appears and
+# outputs it as a dictionary.
+def histogram(input_str:str) -> dict[str, int]:
+    output_dict = {}
+    split = str.split(input_str)
+    for word in split:
+        if word not in output_dict:
+            output_dict[word] = 1
+        else:
+            output_dict[word] += 1
+    return output_dict
--- lab6_tests.py
+++ lab6_tests.py
@@ -2,6 +2,8 @@ import data
 import lab6
 import unittest
 
+from lab6 import selection_sort, selection_sort_alpha
+
 
 # Write your test cases for each part below.
 
@@ -64,16 +66,56 @@ class TestCases(unittest.TestCase):
 
 
     # Part 1
-
+    def test1_selection_sort_alpha(self):
+        input1 = [data.Book(["Jacob"], "Unicycle"), data.Book(["Sophia"], "Flute"),
+                  data.Book(["Travis"], "Soccer")]
+        expected = [data.Book(["Sophia"], "Flute"), data.Book(["Travis"], "Soccer"),
+                    data.Book(["Jacob"], "Unicycle")]
+        lab6.selection_sort_alpha(input1)
+        self.assertEqual(expected, input1)
+    def test2_selection_sort_alpha(self):
+        input1 = [data.Book(["Jacob"], "aabb"), data.Book(["Sophia"], "aaab"),
+                  data.Book(["Travis"], "abbb")]
+        expected = [data.Book(["Sophia"], "aaab"), data.Book(["Jacob"], "aabb"),
+                    data.Book(["Travis"], "abbb")]
+        lab6.selection_sort_alpha(input1)
+        self.assertEqual(expected, input1)
 
     # Part 2
-
+    def test1_swap_case(self):
+        input1 = "Hey There!"
+        result = lab6.swap_case(input1)
+        expected = "hEY tHERE!"
+        self.assertEqual(expected, result)
+    def test2_swap_case(self):
+        input1 = "abcABC123"
+        result = lab6.swap_case(input1)
+        expected = "ABCabc123"
+        self.assertEqual(expected, result)
 
     # Part 3
-
+    def test1_str_translate(self):
+        input1 = "Hey There!"
+        result = lab6.str_translate(input1, "e", "a")
+        expected = "Hay Thara!"
+        self.assertEqual(expected, result)
+    def test2_str_translate(self):
+        input1 = "123321"
+        result = lab6.str_translate(input1, "2", "6")
+        expected = "163361"
+        self.assertEqual(expected, result)
 
     # Part 4
-
+    def test1_histogram(self):
+        input1 = ("Hey there!")
+        result = lab6.histogram(input1)
+        expected = {"Hey":1, "there!":1}
+        self.assertEqual(expected, result)
+    def test2_histogram(self):
+        input1 = ("ooo aaa ooo aaa ooo ooo")
+        result = lab6.histogram(input1)
+        expected = {"ooo":4, "aaa":2}
+        self.assertEqual(expected, result)
 
 
 
